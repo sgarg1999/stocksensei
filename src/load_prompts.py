d8e99a944234a2cb76f8060dcc9dab7dbf776a0b
sgarg1999 <garg.shivam4612@gmail.com>
2023-05-07 15:26:23 -0400

    "Converted relative file path to absolute for compatability"

--- src/load_prompts.py
+++ src/load_prompts.py
@@ -1,10 +1,17 @@
+import os
 from langchain.prompts import PromptTemplate
+
+
 
 def load_prompt_template(prompt_style = 'financial_advisor'):
 
+    prompts_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'data', 'prompts')
+
+
     if prompt_style == 'financial_advisor':
 
-        with open('../data/prompts/fin_advisor_prompt.txt', 'r') as file:
+        with open( os.path.join(prompts_path, 'fin_advisor_prompt.txt'),
+                   'r') as file:
             prompt_template = file.read().replace('\n', '')
 
     return prompt_template
